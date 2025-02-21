859f994f0d56fe99b2f0390c0acde43bd965d98b
chinni <acsrikar@gmail.com>
2025-02-21 17:54:52 +0900

    Obtain text from PDF using tesseract and pymupdf and store it in vector DB

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 - Install requirements with `pip install -r requirements.py`
 - Run `prepare_vectordb.py`
 - Run chatbot with `streamlit run chatbot_ui.py`
+
+## Key points
+
+- Used a full text PDF for base version
+- Using Yugioh rules for testing multimodal PDFs: https://img.yugioh-card.com/en/downloads/rulebook/SD_RuleBook_EN_10.pdf
--- chatbot.py
+++ chatbot.py
@@ -9,16 +9,15 @@ load_dotenv()
 
 class Chatbot:
     def __init__(self, db_folder: str = "./vectorstore"):
-        self.embedding = OpenAIEmbeddings(model="text-embedding-3-small")
+        self.embedding = OpenAIEmbeddings(model="text-embedding-ada-002") # changed to make it faster
 
         if not os.path.exists(db_folder) or len(os.listdir(db_folder)) == 0:
-            raise ValueError("No precomputed ChromaDB found! Run `python prepare_vectordb.py` first.")
+            raise ValueError("No precomputed VectorDB found! Run `python prepare_vectordb.py` first.")
 
         print("Loading precomputed VectorDB...")
         self.vectorstore = Chroma(persist_directory=db_folder, embedding_function=self.embedding)
 
         self.memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True, input_key="question")
-
         self.llm = OpenAI(temperature=0.5, streaming=True)
 
         self.retriever = self.vectorstore.as_retriever(search_type="mmr", search_kwargs={"k": 5, "lambda_mult": 0.5})
